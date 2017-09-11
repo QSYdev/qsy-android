419a624b8d05971583f1dc31b2c3cc179b94781d
Ramiro <ramiroromero94@gmail.com>
2017-09-10 23:08:46 -0300

    Progreso sobre ExecutionActivity

--- app/src/main/java/com/qsy/terminal/ExecutionActivity.java
+++ app/src/main/java/com/qsy/terminal/ExecutionActivity.java
@@ -6,7 +6,12 @@ import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.TextView;
 
 import com.qsy.terminal.services.LibterminalService;
 
@@ -17,6 +22,9 @@ public class ExecutionActivity extends AppCompatActivity implements EventListene
 
 	private LibterminalService libterminalService;
 	private ExecutionActivityConnection mConnection = new ExecutionActivityConnection();
+	private boolean mRoutineWasCanceled = false;
+	private Chronometer mChronometer;
+	private TextView mStatus;
 
 	@Override
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -25,12 +33,69 @@ public class ExecutionActivity extends AppCompatActivity implements EventListene
 
 		Intent intent = new Intent(ExecutionActivity.this, LibterminalService.class);
 		intent.setAction("on");
-		// TODO: chequear si hace falta hacer un "startService"
 		bindService(intent, mConnection, 0);
+
+		Button button = (Button) findViewById(R.id.cancelButton);
+		button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				// TODO: Poner primero un cuadro de confirmación
+				libterminalService.getTerminal().stopExecution();
+				doStopRoutine(true);
+				ExecutionActivity.this.finish();
+			}
+		});
+		mChronometer = (Chronometer) findViewById(R.id.chronometer2);
+		mChronometer.start();
+
+		mStatus = (TextView) findViewById(R.id.statusText);
+		mStatus.setText(R.string.status_running);
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		if (libterminalService != null) {
+			libterminalService.getTerminal().addListener(this);
+		}
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		unbindService(mConnection);
+		if (libterminalService != null) {
+			libterminalService.getTerminal().removeListener(this);
+		}
 	}
 
 	@Override
 	public void receiveEvent(Event event) {
+		/*
+		 * Tener en cuenta que esto se llama desde el hilo de Libterminal, lo cual no se si es
+		 * legal. Si algo raro pasa, mirar acá!
+		 */
+		switch (event.getEventType()) {
+			case routineFinished:
+				runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+					    doStopRoutine(false);
+						AlertDialog.Builder builder = new AlertDialog.Builder(ExecutionActivity.this);
+						builder.setTitle(R.string.routine_finished);
+						builder.setIcon(android.R.drawable.ic_dialog_alert);
+						builder.setNeutralButton(R.string.ok, null);
+						builder.create().show();
+					}
+				});
+				break;
+		}
+	}
+
+	private void doStopRoutine(boolean cancel) {
+		mChronometer.stop();
+		mStatus.setText(R.string.status_finished);
+		mRoutineWasCanceled = cancel;
 	}
 
 	private final class ExecutionActivityConnection implements ServiceConnection {
@@ -45,7 +110,10 @@ public class ExecutionActivity extends AppCompatActivity implements EventListene
 		@Override
 		public void onServiceDisconnected(ComponentName name) {
 			libterminalService.getTerminal().removeListener(ExecutionActivity.this);
-			// TODO: HAY QUE HACER STOP?? O ALCANZA CON MAIN ACTIVITY???
+			/*
+			 * TODO: este método se ejecuta cuando por alguna razón se perdio el Service. Habría que
+			 * manejar esto
+			 */
 		}
 	}
 }
